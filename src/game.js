46d2d57576171d3ea80f6abbc2d25e16bad6889f
Thomas Christlieb <thomaschristlieb@hotmail.com>
2019-01-25 20:39:38 +0100

    more structure

---
+++ src/game.js
@@ -0,0 +1,18 @@
+let config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    parent: 'game',
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 0 },
+            debug: false
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
