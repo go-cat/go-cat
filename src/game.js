c2d4ad15dfb225372a0110ad6dd4f707bc6f274c
Thomas Christlieb <thomaschristlieb@hotmail.com>
2019-01-25 20:50:34 +0100

    Add gravity

--- src/game.js
+++ src/game.js
@@ -6,7 +6,7 @@ let config = {
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: { y: 0 },
+            gravity: { y: 100 },
             debug: false
         }
     },
