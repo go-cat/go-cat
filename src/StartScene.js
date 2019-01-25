78c8593ff9187ec40282de572c6ee4d4e819d434
Bernd Stellwag <burned@zerties.org>
2019-01-25 23:20:57 +0100

    cleanup: add semi colons

--- src/StartScene.js
+++ src/StartScene.js
@@ -4,41 +4,41 @@ class StartScene extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image('logo', 'assets/images/go_cat_icon_transparent.png')
+        this.load.image('logo', 'assets/images/go_cat_icon_transparent.png');
     }
 
     create() {
-        let graphics = this.add.graphics()
-        const color1 = 0xffa435
-        const color2 = 0xc26700
-        const lineHeight = 50
+        let graphics = this.add.graphics();
+        const color1 = 0xffa435;
+        const color2 = 0xc26700;
+        const lineHeight = 50;
         for (let i = 0; i < this.game.config.height / lineHeight; i++) {
-            graphics.fillGradientStyle(color1, color1, color2, color2)
-            graphics.fillRect(0, (i * lineHeight), this.game.config.width, lineHeight * 0.1)
-            graphics.fillStyle(color2)
-            graphics.fillRect(0, ((lineHeight * 0.1) + (i * lineHeight)), this.game.config.width, lineHeight * 0.4)
-            graphics.fillGradientStyle(color2, color2, color1, color1)
-            graphics.fillRect(0, ((lineHeight * 0.5) + (i * lineHeight)), this.game.config.width, lineHeight * 0.1)
-            graphics.fillStyle(color1)
+            graphics.fillGradientStyle(color1, color1, color2, color2);
+            graphics.fillRect(0, (i * lineHeight), this.game.config.width, lineHeight * 0.1);
+            graphics.fillStyle(color2);
+            graphics.fillRect(0, ((lineHeight * 0.1) + (i * lineHeight)), this.game.config.width, lineHeight * 0.4);
+            graphics.fillGradientStyle(color2, color2, color1, color1);
+            graphics.fillRect(0, ((lineHeight * 0.5) + (i * lineHeight)), this.game.config.width, lineHeight * 0.1);
+            graphics.fillStyle(color1);
             graphics.fillRect(0, ((lineHeight * 0.6) + (i * lineHeight)), this.game.config.width, lineHeight * 0.4)
         }
 
-        this.add.circle(400, 300, 150, color1)
-        this.add.image(400, 300, 'logo')
+        this.add.circle(400, 300, 150, color1);
+        this.add.image(400, 300, 'logo');
 
         this.input.keyboard.on('keydown', (event) => {
             switch (event.key) {
                 case '1':
-                    this.scene.start('TreeLevel')
+                    this.scene.start('TreeLevel');
                     break;
                 case '2':
-                    this.scene.start('GrassLevel')
+                    this.scene.start('GrassLevel');
                     break;
                 case '3':
-                    this.scene.start('StreetLevel')
+                    this.scene.start('StreetLevel');
                     break;
                 case '4':
-                    this.scene.start('SecretLevel')
+                    this.scene.start('SecretLevel');
                     break;
             }
         })
--- src/levels/StreetLevel.js
+++ src/levels/StreetLevel.js
@@ -1,14 +1,11 @@
 class StreetLevel extends Phaser.Scene {
-
-    var car;
-
     constructor() {
         super({ key: 'StreetLevel' })
     }
 
     preload() {
-        this.load.image('grass', 'assets/images/StreetLevel/grass.png')
-        this.load.image('car', 'assets/images/StreetLevel/car.png')
+        this.load.image('grass', 'assets/images/StreetLevel/grass.png');
+        this.load.image('car', 'assets/images/StreetLevel/car.png');
         this.load.image('cat', 'assets/images/cat.png')
     }
 
@@ -16,17 +13,17 @@ class StreetLevel extends Phaser.Scene {
         // Our platforms and ground, all static in a group
         let platforms = this.physics.add.staticGroup();
 
-        this.add.image(0, 0, 'grass').setOrigin(0, 0)
+        this.add.image(0, 0, 'grass').setOrigin(0, 0);
 
         this.cat = this.physics.add.sprite(100, 0, 'cat');
         this.cat.setBounce(0.2);
         this.cat.setCollideWorldBounds(true);
-        this.cat.body.setAllowGravity(0,0)
+        this.cat.body.setAllowGravity(0,0);
 
-        this.car = this.physics.add.image(50, 50, 'car')
-        target.x=100
-        target.y=100
-        this.physics.moveToObject(this.car, target, 200)
+        this.car = this.physics.add.image(50, 50, 'car');
+        target.x=100;
+        target.y=100;
+        this.physics.moveToObject(this.car, target, 200);
 
         /* Initialize the keys */
         this.leftKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
