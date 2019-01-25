adf2a6c04e65a0db5b651f48598e6525c2d63767
Bernd Stellwag <burned@zerties.org>
2019-01-25 22:37:29 +0100

    add some background

--- src/StartScene.js
+++ src/StartScene.js
@@ -4,11 +4,24 @@ class StartScene extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image('logo', 'assets/images/go_cat_icon.png')
+        this.load.image('logo', 'assets/images/go_cat_icon_transparent.png')
     }
 
     create() {
-        this.add.image(400, 300, 'logo')
+        let graphics = this.add.graphics()
+        const color1 = 0xffa435
+        const color2 = 0xc26700
+        const lineHeight = 50
+        for (let i = 0; i < this.game.config.height / lineHeight; i++) {
+            graphics.fillGradientStyle(color1, color1, color2, color2)
+            graphics.fillRect(0, (i * lineHeight), this.game.config.width, lineHeight * 0.1)
+            graphics.fillStyle(color2)
+            graphics.fillRect(0, ((lineHeight * 0.1) + (i * lineHeight)), this.game.config.width, lineHeight * 0.4)
+            graphics.fillGradientStyle(color2, color2, color1, color1)
+            graphics.fillRect(0, ((lineHeight * 0.5) + (i * lineHeight)), this.game.config.width, lineHeight * 0.1)
+            graphics.fillStyle(color1)
+            graphics.fillRect(0, ((lineHeight * 0.6) + (i * lineHeight)), this.game.config.width, lineHeight * 0.4)
+        }
 
         this.input.keyboard.on('keydown', (event) => {
             switch (event.key) {
@@ -23,5 +36,7 @@ class StartScene extends Phaser.Scene {
                     break;
             }
         })
+
+        this.add.image(400, 300, 'logo')
     }
 }
