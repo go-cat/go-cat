7093af425be4a09fab922cdd6330a6371a3a049e
Thomas Christlieb <thomaschristlieb@hotmail.com>
2019-01-26 18:46:39 +0100

    More animations, more mice!

--- src/levels/TreeLevel.js
+++ src/levels/TreeLevel.js
@@ -10,12 +10,12 @@ class TreeLevel extends BaseLevelScene {
 
         // Images
         this.load.image('cat', 'assets/images/cat_walking_right.png');
+        this.load.spritesheet('animcat', 'assets/images/cat_walking_animated.png', { frameWidth: 97, frameHeight: 101 });
         this.load.image('cathump', 'assets/images/cats_hump.png');
         this.load.image('branch', 'assets/images/TreeLevel/branch.png');
         this.load.image('ground', 'assets/images/TreeLevel/bottom_green_60px.png');
         this.load.spritesheet('bird', 'assets/images/bird_flying_animated.png', { frameWidth: 30, frameHeight: 30 } );
         this.load.image('mouse', 'assets/images/mouse_left.png');
-        this.load.image('wool', 'assets/images/ball_wool.png');
         this.load.image('birddropping', 'assets/images/bird_dropping.png');
         this.load.image('goal', 'assets/images/house_home_transparent.png');
 
@@ -30,7 +30,7 @@ class TreeLevel extends BaseLevelScene {
         this.music.play();
 
         // This are the bounds of our world
-        const worldheight = this.game.config.height*4;
+        const worldheight = this.game.config.height*10;
         this.physics.world.setBounds(0, 0, this.game.config.width, worldheight, true, true, true, true);
 
         // Background
@@ -58,13 +58,37 @@ class TreeLevel extends BaseLevelScene {
         platforms.create(200, 1600, 'branch');
         platforms.create(700, 1900, 'branch');
         platforms.create(100, 2200, 'branch');
+        platforms.create(600, 2500, 'branch');
+        platforms.create(300, 2700, 'branch');
+        platforms.create(500, 3000, 'branch');
+        platforms.create(10, 3500, 'branch');
+        platforms.create(50, 3800, 'branch');
+        platforms.create(400, 4000, 'branch');
+        platforms.create(700, 4100, 'branch');
+        platforms.create(600, 4500, 'branch');
+        platforms.create(650, 4900, 'branch');
+        platforms.create(200, 5000, 'branch');
+        platforms.create(500, 5200, 'branch');
+        platforms.create(100, 5350, 'branch');
+        platforms.create(300, 5500, 'branch');
 
         // Our cat
-        this.cat = this.physics.add.sprite(100, 0, 'cat');
+        this.cat = this.physics.add.sprite(100, 0, 'animcat');
         this.cat.setBounce(0.2);
         this.cat.setCollideWorldBounds(true);
         this.cameras.main.startFollow(this.cat);
         this.cat.body.gravity.y = 300;
+        this.anims.create({
+            key: 'walk',
+            frames: this.anims.generateFrameNumbers('animcat', { start: 0, end: 3 }),
+            frameRate: 10,
+            repeat: -1,
+        });
+        this.anims.create({
+            key: 'stand',
+            frames: [ { key: 'animcat', frame: 0 } ],
+            frameRate: 20,
+        });
 
         // Bird
         this.bird = this.physics.add.sprite(-100, -100, 'bird');
@@ -77,21 +101,21 @@ class TreeLevel extends BaseLevelScene {
             frames: this.anims.generateFrameNumbers('bird', { start: 0, end: 1 }),
             frameRate: 10,
             repeat: -1
-        });         
+        });
         this.bird.anims.play('fly');
 
         // Birds do poop
         this.birdpoops = this.physics.add.group();
 
         // Mice
-        this.mice = this.physics.add.group({
-            key: 'mouse',
-            repeat: 11,
-            setXY: { x: 12, y: 20, stepX: 120 },
-        });
-        this.mice.children.iterate(function (mouse) {
+        this.mice = this.physics.add.group();
+	    for (let i = 0; i < 100; i++) {
+	    	let x = Phaser.Math.Between(0, this.game.config.width);
+	    	let y = Phaser.Math.Between(600, worldheight);
+            let mouse = this.mice.create(x, y, 'mouse');
             mouse.body.setCollideWorldBounds(true);
-        });
+	    }           
+        
 
         // Colide events
         // The cat
@@ -126,23 +150,11 @@ class TreeLevel extends BaseLevelScene {
     update(time, delta) {
         super.update(time, delta);
 
-        // Mice run
-        this.mice.children.iterate(function (mouse) {
-            let x = Phaser.Math.Between(1, 3);
-            if (x === 1) {
-                mouse.setVelocityX(0);
-            } else if (x === 2) {
-                mouse.setVelocityX(-500);
-            } else {
-                mouse.setVelocityX(500);
-            }
-        });
-
         // Birds fly
         /* Do we have a bird? */
         if (this.bird.flying) {
             /* should he poop? */
-            if (Phaser.Math.Between(1, 100) < 3) {
+            if (Phaser.Math.Between(1, 100) < 3 && this.bird.y > 250) {
                 let birdpoop = this.birdpoops.create(this.bird.x, this.bird.y, 'birddropping').setScale(2).refreshBody();
                 birdpoop.setBounceY(0);
                 birdpoop.body.allowGravity = true;
@@ -172,8 +184,10 @@ class TreeLevel extends BaseLevelScene {
     buttonPressedLeft(pressed) {
         if (pressed) {
             this.cat.setVelocityX(-500);
+            this.cat.anims.play('walk', true);
         } else {
             this.cat.setVelocityX(0);
+            this.cat.anims.play('stand');
         }
 
         if (this.cat.flipX === false) {
@@ -184,8 +198,10 @@ class TreeLevel extends BaseLevelScene {
     buttonPressedRight(pressed) {
         if (pressed) {
             this.cat.setVelocityX(500);
+            this.cat.anims.play('walk', true);
         } else {
             this.cat.setVelocityX(0);
+            this.cat.anims.play('stand', true);
         }
 
         if (this.cat.flipX === true) {
