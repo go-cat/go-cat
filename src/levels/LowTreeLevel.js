314ad2dc9bca1f3e57846e0f665800b97957911f
Markus Kostrzewski <ciapa@ciapa.tech>
2019-01-27 13:12:43 +0100

    Fix and improve LowTreeLevel

--- src/levels/LowTreeLevel.js
+++ src/levels/LowTreeLevel.js
@@ -12,7 +12,7 @@ class LowTreeLevel extends BaseLevelScene {
         this.load.tilemapTiledJSON("lowTree_map","assets/maps/LowTreeLevel/world.json");
 
         // Images
-        this.load.image('tiles',"assets/images/LowTreeLevel/lowTreeTileset.png");
+        this.load.image('lowTree_tiles',"assets/images/LowTreeLevel/lowTreeTileset.png");
         this.load.image('mouse', 'assets/images/mouse_left.png');
         this.load.image('cat', 'assets/images/cat_walking_right.png');
         this.load.image('home', 'assets/images/house_home_transparent.png');
@@ -41,7 +41,7 @@ class LowTreeLevel extends BaseLevelScene {
     
         // layer and map for the Tilemap
         let map = this.make.tilemap({ key: "lowTree_map", tileWidth: 32, tileHeight: 32 });
-        let tileset = map.addTilesetImage("lowTreeTileset","tiles");
+        let tileset = map.addTilesetImage("lowTreeTileset","lowTree_tiles");
 
         let dynamicLayer = map.createDynamicLayer("background", tileset, 0, 0);
         let collisionLayer = map.createStaticLayer("obstacles", tileset, 0, 0);
@@ -52,9 +52,6 @@ class LowTreeLevel extends BaseLevelScene {
         // bounds
         this.physics.world.setBounds(0, 0, 800, 2400, true, true, true, true);
         this.cameras.main.setBounds(0, 0, 800, 2400);
-
-        // Variables
-        this.millis = 0;
 
 
         // "Read" the Object-Layers
@@ -75,18 +72,13 @@ class LowTreeLevel extends BaseLevelScene {
         })[0];
 
 
-        // Our goal
-        this.goal = this.physics.add.image(688, 2320, 'goal');
-        this.goal.body.setAllowGravity(0, 0);
-
-
         // Our cat
-        this.cat = this.physics.add.sprite(352, 64, 'animcat');
+        this.cat = this.physics.add.sprite(384, 64, 'animcat');
         //this.cat.setBounce(0.2);
         this.cat.setCollideWorldBounds(true);
-        this.cat.body.gravity.y = 300;
-        //this.cat.scaleY=1;
-        //this.cat.scaleX=1;
+        this.cat.body.gravity.y = 500;
+        this.cat.scaleY=0.7;
+        this.cat.scaleX=0.7;
 
         // Cat animations
         this.anims.remove('walk');
@@ -107,20 +99,22 @@ class LowTreeLevel extends BaseLevelScene {
 
         // Flying Birds
         this.birds = this.physics.add.group();
-        this.birdAreas = []
+        this.birdAreas = [];
         for (let i = 0; i < this.birdSpawnLayer.objects.length; i++) {
-          let areaStartX, areaSpeed;
-          let areaStartY = this.birdSpawnLayer.objects[i].y-32;
-          if (this.miceSpawnLayer.objects[i].name === "left") {
-            // flying to the left
-            areaStartX = this.birdSpawnLayer.objects[i].x + this.birdSpawnLayer.objects[i].width;
-            areaSpeed = Phaser.Math.Between(-50, -80);
-          } else {
-            // flying to the right
-            areaStartX = this.birdSpawnLayer.objects[i].x;
-            areaSpeed = Phaser.Math.Between(50, 80);
-          }
-          this.birdAreas.push({"startX" : areaStartX, "startY" : areaStartY, "speed" : areaSpeed});
+            let curSpawnArea = this.birdSpawnLayer.objects[i];
+              let areaStartX;
+              let areaStartY = curSpawnArea.y + (curSpawnArea.height / 2);
+              let areaSpeed = curSpawnArea.properties[1].value;
+              let areaSpawnTime = curSpawnArea.properties[0].value;
+
+              if (areaSpeed < 0) {
+                // flying to the left
+                areaStartX = curSpawnArea.x + curSpawnArea.width;
+              } else {
+                // flying to the right
+                areaStartX = curSpawnArea.x;
+              }
+              this.birdAreas.push({"startX" : areaStartX, "startY" : areaStartY, "speed" : areaSpeed, "spawnTime" : areaSpawnTime, "curTime" : 0});
         }
 
         // Bird animation
@@ -138,7 +132,7 @@ class LowTreeLevel extends BaseLevelScene {
         this.miceSprites = this.physics.add.group();
         for(let i = 0; i < this.miceSpawnLayer.objects.length; i++){
             let mouseStartX = this.miceSpawnLayer.objects[i].x ;
-            let mouseStartY = this.miceSpawnLayer.objects[i].y-20;
+            let mouseStartY = this.miceSpawnLayer.objects[i].y+20;
             let mousePath = this.miceSpawnLayer.objects[i].width;
             let mouseSpeed = Phaser.Math.Between(50, 80);
             let sprite = this.physics.add.sprite(mouseStartX, mouseStartY,"mouse");
@@ -159,11 +153,12 @@ class LowTreeLevel extends BaseLevelScene {
 
 
         // Decorative Elements
-        this.decorativeElements = this.physics.add.staticGroup();
+        this.decorativeElements = this.physics.add.group();
         // Decorative bird droppings
         for(let i = 0; i < this.birdDroppingsLayer.objects.length; i++) {
             let drop = this.birdDroppingsLayer.objects[i];
-            let dropEl = this.decorativeElements.create(drop.x, drop.y, 'birddropping');
+            let dropEl = this.decorativeElements.create(drop.x + (drop.width / 2), drop.y, 'birddropping');
+            dropEl.setScale(Phaser.Math.Between(1.8, 2.1));
             dropEl.flipX = Phaser.Math.Between(0, 10) > 5;
         }
         // Decorative static birds
@@ -176,9 +171,10 @@ class LowTreeLevel extends BaseLevelScene {
 
         // Collide events
         // The cat
+        this.physics.add.collider(this.decorativeElements, collisionLayer);
         this.physics.add.collider(this.cat, collisionLayer);
         this.physics.add.collider(this.miceSprites, collisionLayer);
-        this.physics.add.collider(this.cat, this.birdSprites, () => {
+        this.physics.add.collider(this.cat, this.birds, () => {
             this.catDies(this.cat);
         });
 
@@ -190,14 +186,6 @@ class LowTreeLevel extends BaseLevelScene {
             this.startNextLevel();
         }, null, this);
 
-
-        /* //in case the way up there (with the safezone) does not work
-        this.physics.add.collider(this.cat, this.goal, () => {
-            this.addScore(100);
-            this.addScore(Math.floor(this.timeLeft));
-            this.startNextLevel();
-        });
-        */
 
         this.cameras.main.startFollow(this.cat);
 
@@ -217,24 +205,23 @@ class LowTreeLevel extends BaseLevelScene {
         }
 
 
-        // Spawning birds regularly
-        let delay = 1500;
-        if (this.millis > delay){
-            for (let i = 0; i < this.birdAreas.length; i++) {
-                let curArea = this.birdAreas[i];
-            		let bird = this.birds.create(curArea["startX"], curArea["startY"], 'bird').setScale(2);
-            		bird.setBounceY(0);
+        // Spawning birds
+        for (let i = 0; i < this.birdAreas.length; i++) {
+            let curArea = this.birdAreas[i];
+            if (curArea["curTime"] <= 0) {
+                curArea["curTime"] += curArea["spawnTime"] * 100;
+                let bird = this.birds.create(curArea["startX"], curArea["startY"], 'bird').setScale(1.2);
+                bird.setBounceY(0);
                 bird.setVelocityX(curArea["speed"]);
-                bird.flipX = curArea["speed"] < 0;
+                bird.flipX = curArea["speed"] > 0;
                 bird.flying = false;
-            		bird.body.allowGravity = false;
-            		bird.body.setCollideWorldBounds(false);
+                bird.body.allowGravity = false;
+                bird.body.setCollideWorldBounds(false);
 
-            		bird.anims.play('fly');
+                bird.anims.play('fly');
             }
-            this.millis -= delay;
+            curArea["curTime"] -= delta * Math.abs(curArea["speed"]) / 10;
         }
-        this.millis += delta;
 
 
         // Mice move
@@ -258,7 +245,7 @@ class LowTreeLevel extends BaseLevelScene {
 
     buttonPressedLeft(pressed) {
         if (pressed) {
-            this.cat.setVelocityX(-500);
+            this.cat.setVelocityX(-200);
             this.cat.anims.play('walk', true);
         } else {
             this.cat.setVelocityX(0);
@@ -272,7 +259,7 @@ class LowTreeLevel extends BaseLevelScene {
 
     buttonPressedRight(pressed) {
         if (pressed) {
-            this.cat.setVelocityX(500);
+            this.cat.setVelocityX(200);
             this.cat.anims.play('walk', true);
         } else {
             this.cat.setVelocityX(0);
@@ -286,7 +273,7 @@ class LowTreeLevel extends BaseLevelScene {
 
     buttonPressedUp(pressed) {
         if (pressed && Math.abs(this.cat.body.velocity.y) < 2) {
-            this.cat.setVelocityY(-350);
+            this.cat.setVelocityY(-420);
         }
     }
 
